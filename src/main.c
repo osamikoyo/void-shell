ea94c6a9a2a84feed8473902fdc5c8f017899c17
osamikoyo <v.pylayev@mail.ru>
2025-03-31 06:26:53 +0500

    add welcome

--- src/main.c
+++ src/main.c
@@ -5,21 +5,33 @@
 
 void cmd_route(char *cmd) {}
 
+char *cd_route(char *cmd) {}
+
 int main(int argc, char *argv[]) {
   bool interacrive;
 
   char *filepath = calloc(30, sizeof(char));
+  char *current_dir = calloc(50, sizeof(char));
+  char *welcome = calloc(70, sizeof(char));
+
+  strcpy("~", current_dir);
 
   if (argc < 2) {
     interacrive = true;
   } else {
-    strcpy(filepath, argv[1]);
+    strcpy(argv[1], filepath);
   }
 
   bool ok = true;
 
   while (ok) {
+    sprintf(welcome, "%s\n%s", current_dir, "void_|||> ");
+
+    printf("%s", welcome);
   }
+
+  free(current_dir);
+  free(filepath);
 
   return 0;
 }
